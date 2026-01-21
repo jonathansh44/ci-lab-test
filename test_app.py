2a9365181643f74e2c9d72bc06f7c44be5ea485e
jonathansh44 <yoni123456747@gmail.com>
2026-01-21 21:16:29 +0200

    new files

---
+++ test_app.py
@@ -0,0 +1,19 @@
+import unittest
+import app
+
+class TestApp(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(app.add(2, 3), 5)
+
+    def test_subtract(self):
+        self.assertEqual(app.subtract(10, 5), 5)
+
+    def test_multiply(self):
+        self.assertEqual(app.multiply(3, 4), 12)
+
+    def test_divide(self):
+        self.assertEqual(app.divide(10, 2), 5)
+
+if __name__ == '__main__':
+    unittest.main()
